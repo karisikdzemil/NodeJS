542e4606f5da99aebb9cef385526b6a583899c79
dzemil karisik <karisikdzemil@gmail.com>
2025-08-03 09:08:15 +0200

    templating engines - working with pug

--- express/app.js
+++ express/app.js
@@ -1,13 +1,15 @@
 const path = require("path");
 
 const express = require("express");
-
-const adminData = require("./routes/admin");
-const shopRoutes = require("./routes/store");
-
 const bodyParser = require('body-parser');
 
 const app = express();
+
+app.set("view engine", "pug");
+app.set("views", "views");
+
+const adminData = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, "public")));
