15f57d3885e73713fdcdd46fa0d134066f135576
dzemil karisik <karisikdzemil@gmail.com>
2025-08-04 21:28:25 +0200

    the app enhanced

--- express/routes/admin.js
+++ express/routes/admin.js
@@ -2,14 +2,17 @@ const path = require("path");
 
 const express = require("express");
 
-const productsController = require("../controllers/products")
+const adminControler = require("../controllers/admin")
 
 const router = express.Router();
  
 // admin/add-product => GET
-router.get("/add-product", productsController.getAddProduct);
+router.get("/add-product", adminControler.getAddProduct);
+
+// admin/products => GET
+router.get("/products");
 
 // admin/add-product => POST
-router.post("/add-product", productsController.postAddProduct);
+router.post("/add-product", adminControler.postAddProduct);
 
 module.exports = router;
