d36ab06462433ff7da9ce74dfa255455c43a3047
dzemil karisik <karisikdzemil@gmail.com>
2025-08-24 21:34:44 +0200

    started with mvc

--- ToDo/app.js
+++ ToDo/app.js
@@ -9,15 +9,15 @@ app.set("views, views");
 app.use(express.static("public"));
 
 app.get("/add-todo", (req, res, next) => {
-    res.render("newTodo", {path: "/add-todo"})
+    res.render("newTodo", {path: "/add-todo", title: "Add ToDo"})
 });
 
 app.get("/all-todo", (req, res, next) => {
-    res.render("allTodos", {path: "/all-todo"})
+    res.render("allTodos", {path: "/all-todo", title: "All ToDo's"})
 })
 
 app.get("/", (req, res) => {
-    res.render('home', {path: "/"})
+    res.render('home', {path: "/", title: "ToDo App"})
 });
 
 app.listen(3000);
