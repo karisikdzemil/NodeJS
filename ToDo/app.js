9c81994c61631e7b22c0c4d842f2dd12c2cf4de3
dzemil karisik <karisikdzemil@gmail.com>
2025-08-24 17:56:44 +0200

    todo exercise style core pages and logic

--- ToDo/app.js
+++ ToDo/app.js
@@ -6,10 +6,18 @@ const app = express();
 app.set("view engine", "ejs");
 app.set("views, views");
 
-app.use(express.static("public"))
+app.use(express.static("public"));
+
+app.get("/add-todo", (req, res, next) => {
+    res.render("newTodo", {path: "/add-todo"})
+});
+
+app.get("/all-todo", (req, res, next) => {
+    res.render("allTodos", {path: "/all-todo"})
+})
 
 app.get("/", (req, res) => {
-    res.render('home')
+    res.render('home', {path: "/"})
 });
 
 app.listen(3000);
