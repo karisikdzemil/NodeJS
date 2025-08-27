3b1427efadbb1b27f01867a29a55e0c8996ab99c
dzemil karisik <karisikdzemil@gmail.com>
2025-08-27 19:30:50 +0200

    connected my ToDo exercise with database

--- ToDo/app.js
+++ ToDo/app.js
@@ -1,14 +1,24 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
 app.set("view engine", "ejs");
 app.set("views, views");
 
+const db = require("./util/database");
+
 app.use(express.static("public"));
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 const todoRoutes = require("./routes/todoRoutes");
+
+db.execute("SELECT * FROM ToDo.todo")
+  .then((res) => {
+    console.log(res);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.use(todoRoutes);
 
