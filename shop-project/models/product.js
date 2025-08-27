6768e3c2f8338e6944f11e1e24beff8b9fb47d2d
dzemil karisik <karisikdzemil@gmail.com>
2025-08-27 18:37:22 +0200

    inserting data and fetching a single product

--- shop-project/models/product.js
+++ shop-project/models/product.js
@@ -1,4 +1,4 @@
-const db = require('../util/database')
+const db = require('../util/database');
 
 const Cart = require('./cart');
 
@@ -12,18 +12,19 @@ module.exports = class Product {
   }
 
   save() {
+    return db.execute(
+      'INSERT INTO products (title, price, imageUrl, description) VALUES (?, ?, ?, ?)',
+      [this.title, this.price, this.imageUrl, this.description]
+    );
+  }
+
+  static deleteById(id) {}
+
+  static fetchAll() {
     return db.execute('SELECT * FROM products');
   }
 
-  static deleteById(id) {
-   
-  }
-
-  static fetchAll() {
-
-  }
-
   static findById(id) {
-   
+    return db.execute('SELECT * FROM products WHERE products.id = ?', [id]);
   }
 };
