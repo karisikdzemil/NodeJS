948cffd277de578ba3e3b09bc50da20417b2d7a3
dzemil karisik <karisikdzemil@gmail.com>
2025-09-03 18:42:23 +0200

    preparing project for mongodb

--- shop-project/util/database.js
+++ shop-project/util/database.js
@@ -1,15 +1,29 @@
 const mongodb = require("mongodb");
 const MongoClient = mongodb.MongoClient;
 
+let _db;
+
 const mongoConnect = (callback) => {
   MongoClient.connect(
-    "mongodb+srv://karisikdzemil:Dzemil123@cluster0.ldrhrp1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    "mongodb+srv://karisikdzemil:Dzemil123@cluster0.ldrhrp1.mongodb.net/shop?retryWrites=true&w=majority&appName=Cluster0"
   )
     .then((client) => {
       console.log("Connected!!!");
-      callback(client)
+      _db = client.db();
+      callback(client);
     })
-    .catch((err) => console.log(err));
+    .catch((err) => {
+      console.log(err);
+      throw err;
+    });
+};
+
+const getDb = () => {
+  if(_db) {
+    return _db;
+  }
+  throw 'No database found!';
 }
 
-module.exports = mongoConnect;
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
