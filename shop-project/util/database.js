a737b0a5da832a7076d6e034d1a0a78520a764ed
dzemil karisik <karisikdzemil@gmail.com>
2025-09-03 08:27:01 +0200

    database connection

--- shop-project/util/database.js
+++ shop-project/util/database.js
@@ -1,10 +1,15 @@
 const mongodb = require("mongodb");
 const MongoClient = mongodb.MongoClient;
 
-MongoClient.connect(
-  "mongodb+srv://karisikdzemil:Dzemil123@cluster0.ldrhrp1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-)
-  .then((result) => {
-    console.log("Connected!!!");
-  })
-  .catch((err) => console.log(err));
+const mongoConnect = (callback) => {
+  MongoClient.connect(
+    "mongodb+srv://karisikdzemil:Dzemil123@cluster0.ldrhrp1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+  )
+    .then((client) => {
+      console.log("Connected!!!");
+      callback(client)
+    })
+    .catch((err) => console.log(err));
+}
+
+module.exports = mongoConnect;
