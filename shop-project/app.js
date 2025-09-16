ed3663794e586e8c0ba695f1744d459d3aa412b4
dzemil karisik <karisikdzemil@gmail.com>
2025-09-16 16:50:22 +0200

    storing users and creating cart

--- shop-project/app.js
+++ shop-project/app.js
@@ -21,11 +21,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
   User.findById('68c07f78c4e69f455f24c275')
     .then(user => {
-      req.user = user;
+      req.user = new User(user.name, user.email, user.cart, user._id);
       next();
     })
     .catch(err => console.log(err));
-  next();
 });
 
 app.use('/admin', adminRoutes);
