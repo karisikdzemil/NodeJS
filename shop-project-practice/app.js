6aded743e469536978121d0a91620777bcfed4f6
dzemil karisik <karisikdzemil@gmail.com>
2025-09-18 21:14:54 +0200

    added more routes

--- shop-project-practice/app.js
+++ shop-project-practice/app.js
@@ -3,8 +3,17 @@ const express = require('express');
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send('kako si ')
+    res.send('Home')
+});
+
+app.get('/products', (req, res) => {
+    res.send('Products page');
+});
+
+app.get('/create-product', (req, res) => {
+    res.send('Create product page')
 })
+
 
 app.listen(3000, () => {
     console.log("Sve radi bebiii")
