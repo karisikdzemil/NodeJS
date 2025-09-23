3e0b4e1969aeda6193da745ba899bd95900763fa
dzemil karisik <karisikdzemil@gmail.com>
2025-09-23 21:37:03 +0200

    connected with ejs

--- shop-project-practice/app.js
+++ shop-project-practice/app.js
@@ -2,12 +2,15 @@ const express = require('express');
 
 const app = express();
 
+app.set("view engine", "ejs");
+app.set("views", "views");
+
 app.get("/", (req, res) => {
     res.send('Home')
 });
 
 app.get('/products', (req, res) => {
-    res.send('Products page');
+    res.render('shop/products')
 });
 
 app.get('/create-product', (req, res) => {
