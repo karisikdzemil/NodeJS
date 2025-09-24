2d27e0312049f18c1bcf95e0d4388faddbf0d27f
dzemil karisik <karisikdzemil@gmail.com>
2025-09-24 22:04:21 +0200

    set colro to active link

--- shop-project-practice/app.js
+++ shop-project-practice/app.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
@@ -8,29 +8,29 @@ app.set("views", "views");
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-    res.render('shop/products', {title: "Home Page"});
+  res.render("shop/products", { title: "Home Page", active: 'home' });
 });
 
-app.get('/products', (req, res) => {
-    res.render('shop/products', {title: 'Products Page'})
+app.get("/products", (req, res) => {
+  res.render("shop/products", { title: "Products Page", active: "products" });
 });
 
-app.get('/add-product', (req, res) => {
-    res.render('shop/addProduct', {title: 'Add Product'})
+app.get("/add-product", (req, res) => {
+  res.render("shop/addProduct", { title: "Add Product", active: 'addProduct' });
 });
 
-app.get('/cart', (req, res) => {
-    res.render('shop/cart', {title: 'Cart'});
+app.get("/cart", (req, res) => {
+  res.render("shop/cart", { title: "Cart", active: 'cart' });
 });
 
-app.get('/orders', (req, res) => {
-    res.render('shop/orders', {title: 'Orders'})
-})
+app.get("/orders", (req, res) => {
+  res.render("shop/orders", { title: "Orders", active: 'orders' });
+});
 
-app.get('/admin-products', (req, res) => {
-    res.render('admin/adminProducts', {title: "test test es"});
+app.get("/admin-products", (req, res) => {
+  res.render("admin/adminProducts", { title: "Admin Products", active: 'adminProducts' });
 });
 
 app.listen(3000, () => {
-    console.log("Sve radi bebiii")
+  console.log("Sve radi bebiii");
 });
