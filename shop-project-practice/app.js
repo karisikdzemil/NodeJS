b48ab98f5b8c5e9966e52f5ca73529552ee6ffa8
dzemil karisik <karisikdzemil@gmail.com>
2025-09-24 21:41:16 +0200

    connected views and implemented all core routes

--- shop-project-practice/app.js
+++ shop-project-practice/app.js
@@ -5,20 +5,30 @@ const app = express();
 app.set("view engine", "ejs");
 app.set("views", "views");
 
+app.use(express.static("public"));
+
 app.get("/", (req, res) => {
-    res.render('shop/products');
+    res.render('shop/products', {title: "Home Page"});
 });
 
 app.get('/products', (req, res) => {
-    res.render('shop/products')
+    res.render('shop/products', {title: 'Products Page'})
 });
 
 app.get('/add-product', (req, res) => {
-    res.render('shop/addProduct')
+    res.render('shop/addProduct', {title: 'Add Product'})
 });
 
+app.get('/cart', (req, res) => {
+    res.render('shop/cart', {title: 'Cart'});
+});
+
+app.get('/orders', (req, res) => {
+    res.render('shop/orders', {title: 'Orders'})
+})
+
 app.get('/admin-products', (req, res) => {
-    res.render('admin/adminProducts');
+    res.render('admin/adminProducts', {title: "test test es"});
 });
 
 app.listen(3000, () => {
