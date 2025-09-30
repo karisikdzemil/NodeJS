cafb19f3de0ebbad099471e8097f9f8fa0704c0f
dzemil karisik <karisikdzemil@gmail.com>
2025-09-30 18:36:12 +0200

    added and used user model

--- shop-project/app.js
+++ shop-project/app.js
@@ -4,7 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const errorController = require("./controllers/error");
-// const User = require("./models/user");
+const User = require("./models/user");
 const mongoose = require("mongoose");
 
 const app = express();
@@ -18,14 +18,14 @@ const shopRoutes = require("./routes/shop");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-// app.use((req, res, next) => {
-//   User.findById("68c07f78c4e69f455f24c275")
-//     .then((user) => {
-//       req.user = new User(user.name, user.email, user.cart, user._id);
-//       next();
-//     })
-//     .catch((err) => console.log(err));
-// });
+app.use((req, res, next) => {
+  User.findById("68dc06cb642431c483e38533")
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => console.log(err));
+});
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
@@ -36,5 +36,18 @@ mongoose
   .connect(
     "mongodb+srv://karisikdzemil:Dzemil123@cluster0.ldrhrp1.mongodb.net/shop?retryWrites=true&w=majority&appName=Cluster0"
   )
-  .then((result) => app.listen(3000))
+  .then((result) => {
+    User.findOne().then(user => {
+      if(!user){
+        const user = new User({
+          name: 'Max',
+          email: 'max@test.com',
+          cart: {
+            items: []
+          }
+        });
+        user.save();
+      }
+    })
+    app.listen(3000)})
   .catch((err) => console.log(err));
