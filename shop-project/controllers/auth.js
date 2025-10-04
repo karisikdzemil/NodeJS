ceb98e90016e33089b9a923b720f1656a3b2eb1f
dzemil karisik <karisikdzemil@gmail.com>
2025-10-04 21:14:27 +0200

    flash messages for redirecting

--- shop-project/controllers/auth.js
+++ shop-project/controllers/auth.js
@@ -3,17 +3,30 @@ const bcrypt = require("bcryptjs");
 const User = require("../models/user");
 
 exports.getLogin = (req, res, next) => {
+  let message = req.flash('error');
+  if(message.length > 0){
+    message = message[0];
+  }else{
+    message = null;
+  }
   res.render("auth/login", {
     path: "/login",
     pageTitle: "Login",
-    errorMessage: req.flash('error')
+    errorMessage: message,
   });
 };
 
 exports.getSignup = (req, res, next) => {
+  let message = req.flash('error');
+  if(message.length > 0){
+    message = message[0];
+  }else{
+    message = null;
+  }
   res.render("auth/signup", {
     path: "/signup",
     pageTitle: "Signup",
+    errorMessage: message
   });
 };
 
@@ -32,6 +45,7 @@ exports.postLogin = (req, res, next) => {
           if (doMatch) {
             res.redirect("/");
           }
+          req.flash('error', 'Invalid email or password.');
           res.redirect("/login");
         })
         .catch((err) => {
@@ -48,6 +62,7 @@ exports.postSignup = (req, res, next) => {
   User.findOne({ email: email })
     .than((userDoc) => {
       if (userDoc) {
+        req.flash('error', 'E-Mail exists already, please pick a different one.');
         return res.redirect("/signup");
       }
       return bcrypt
