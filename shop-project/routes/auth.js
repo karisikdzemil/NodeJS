609d46c7c58186821a07269ca97b2183c683a88e
dzemil karisik <karisikdzemil@gmail.com>
2025-10-12 21:20:54 +0200

    adding conditional css classes and keeping user input

--- shop-project/routes/auth.js
+++ shop-project/routes/auth.js
@@ -2,6 +2,7 @@ const express = require("express");
 
 const authController = require("../controllers/auth");
 const { check } = require("express-validator/check");
+const User = require('../models/user');
 
 const router = express.Router();
 
@@ -18,17 +19,30 @@ router.post(
       .isEmail()
       .withMessage("Please enter a valid email.")
       .custom((value, { req }) => {
-        if (value === "test@test.com") {
-          throw new Error("This email address is forbidden.");
-        }
-        return true;
-      }),
+        // if (value === "test@test.com") {
+        //   throw new Error("This email address is forbidden.");
+        // }
+        // return true;
+
+        return User.findOne({ email: value })
+            .then(userDoc => {
+              if (userDoc) {
+                return Promise.reject('E-Mail exists already, please pick a different one.');
+              }
+
+      })}),
     body(
       "password",
       "Please enter a password with only numbers and text and at least 5 characters."
     )
       .isLength({ min: 5 })
-      .isAlpfanumeric()
+      .isAlpfanumeric(),
+      body('confirmPassword').custom((value, {req}) => {
+        if(value === req.body.password){
+            throw new Error('Passwords have to match!')
+        }
+        return true;
+      })
   ],
   authController.postSignup
 );
