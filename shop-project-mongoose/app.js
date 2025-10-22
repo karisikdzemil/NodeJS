4907112a1c09a014d133f85d2b2e962ee90faa42
dzemil karisik <karisikdzemil@gmail.com>
2025-10-22 07:33:47 +0200

    created get routes for views and basic logic

--- shop-project-mongoose/app.js
+++ shop-project-mongoose/app.js
@@ -1,9 +1,35 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
+
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/',(req, res, next) => {
-    res.send('Ovo je moj prvi kod ')
-})
+    res.render('shop/shop', {active: 'home', title: 'Shop'});
+});
+
+app.get('/products', (req, res, next) => {
+    res.render('products', {active: 'products', title: 'Products'});
+});
+
+app.get('/cart', (req, res, next) => {
+    res.render('cart', {active: 'cart', title: 'Cart'});
+});
+
+app.get('/orders', (req, res, next) => {
+    res.render('order', {active: 'order', title: 'Order'});
+});
+
+app.get('/add-product', (req, res, next) => {
+    res.render('add-roduct', {active: 'add-product', title: 'Add Product'});
+});
+
+app.get('/admin-products', (req, res, next) => {
+    res.render('admin-products', {active: 'admin-products', title: 'Admin Products'});
+});
 
 app.listen(3000);
