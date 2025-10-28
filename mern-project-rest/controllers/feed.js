a6f52d33148f09de16dd30c1e553a181e8a8b010
dzemil karisik <karisikdzemil@gmail.com>
2025-10-28 23:03:11 +0100

    static image and error handling

--- mern-project-rest/controllers/feed.js
+++ mern-project-rest/controllers/feed.js
@@ -1,4 +1,6 @@
-const { validationResult } = require('express/validator/check')
+const { validationResult } = require("express/validator/check");
+
+const Post = require("../models/post");
 
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
@@ -9,24 +11,40 @@ exports.getPosts = (req, res, next) => {
         content: "This is the first post!",
         image: "images/image.png",
         creator: {
-          name: 'Dzemil'
+          name: "Dzemil",
         },
-        createdAt: new Date()
-      }
-    ]
+        createdAt: new Date(),
+      },
+    ],
   });
 };
 
 exports.createPost = (req, res, next) => {
   const errors = validationResult(req);
-  if(!errors.isEmpty()){
-    return res.status(422).json({message: 'Validation failed, entered data is incorect', errors: errors.array(  )})
+  if (!errors.isEmpty()) {
+    const error = new Error('Validation failed, entered data is incorect');
+    error.statusCode = 422;
+    throw error;
   }
   const title = req.body.title;
   const content = req.body.content;
-  // Create post in dbo
-  res.status(201).json({
-    message: "Post created successfully!",
-    post: { _id: new Date().toISOString(), title: title, content: content, creator: {name: 'Dzemil'}, createdAt: new Date() },
+  const post = new Post({
+      title: title,
+      content: content,
+      imageUrl: 'images/image.png',
+      creator: { name: "Dzemil" },
   });
+  post.save().then(result => {
+    console.log(result)
+    res.status(201).json({
+    message: "Post created successfully!",
+    post: result,
+  });
+  }).catch(err => {
+    if(!err.statusCode){
+      err.statusCode = 500;
+    }
+    next(err);
+  })
+  
 };
