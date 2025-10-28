bcfac03b6f7adba7879a6c0075520b1cdd217a6c
dzemil karisik <karisikdzemil@gmail.com>
2025-10-28 21:09:29 +0100

    fetching lists of posts

--- mern-project-rest/controllers/feed.js
+++ mern-project-rest/controllers/feed.js
@@ -1,15 +1,26 @@
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
-    posts: [{ title: 'First Post', content: 'This is the first post!' }]
+    posts: [
+      {
+        _id: 1,
+        title: "First Post",
+        content: "This is the first post!",
+        image: "images/image.png",
+        creator: {
+          name: 'Dzemil'
+        },
+        createdAt: new Date()
+      }
+    ]
   });
 };
 
 exports.createPost = (req, res, next) => {
   const title = req.body.title;
   const content = req.body.content;
-  // Create post in db
+  // Create post in dbo
   res.status(201).json({
-    message: 'Post created successfully!',
-    post: { id: new Date().toISOString(), title: title, content: content }
+    message: "Post created successfully!",
+    post: { id: new Date().toISOString(), title: title, content: content },
   });
 };
