445fa0293e29a5410b40fefdbf2c378bc0ee23a3
dzemil karisik <karisikdzemil@gmail.com>
2025-10-29 21:35:52 +0100

    added pagination

--- mern-project-rest/controllers/feed.js
+++ mern-project-rest/controllers/feed.js
@@ -5,14 +5,23 @@ const path = require('path')
 const Post = require("../models/post");
 
 exports.getPosts = (req, res, next) => {
-  Post.find().then(posts => {
-    res.status(200).json({message: 'Fetched posts successfully!', posts: posts})
+  const currentPage = req.query.page || 1;
+  const perPage = 2;
+  Post.countDocuments().then(count => {
+    totalItems = count;
+    return Post.find()
+    .skip((currentPage - 1) * perPage)
+    .limit(perPage)
+  })
+  .then(posts => {
+    res.status(200).json({message: 'Fetched posts successfully!', posts: posts, totalItems: totalItems})
   }).catch(err => {
-    if(!err.statusCode){
+     if(!err.statusCode){
       err.statusCode = 500;
     }
     next(err);
   })
+  
 };
 
 exports.createPost = (req, res, next) => {
