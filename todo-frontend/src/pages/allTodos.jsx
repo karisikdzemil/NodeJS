ea91a84b8989b2c3ab79bc9f021404c351f66921
dzemil karisik <karisikdzemil@gmail.com>
2025-11-06 21:29:38 +0100

    displayed todos fron database

--- todo-frontend/src/pages/allTodos.jsx
+++ todo-frontend/src/pages/allTodos.jsx
@@ -1,7 +1,33 @@
-export default function AllTodos (){
-    return(
-        <section className="p-25 flex flex-col items-center justify-center">
-            <h1 className="text-4xl font-bold">All ToDo's</h1>
-        </section>
-    )
+import { useEffect, useState } from "react";
+
+export default function AllTodos() {
+  const [allTodos, setAllTodos] = useState([]);
+  useEffect(() => {
+    const getToDos = async () => {
+      try {
+        const response = await fetch(
+          "http://localhost:8080/api/todo/all-todos"
+        );
+        const data = await response.json();
+        setAllTodos(data.todos);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getToDos();
+  }, []);
+
+  return (
+    <section className="p-25 flex flex-col items-center justify-center">
+      <h1 className="text-4xl font-bold">All ToDo's</h1>
+      <ul>
+        {allTodos && allTodos.map((todo) => (
+          <li key={todo._id}>
+            <p>{todo.title}</p>
+            <p>{todo.description}</p>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
 }
